0992c3e45e301b0e5162dd9831bc0f89da42e557
aks <dhpdgwmqo98oafdo@gmail.com>
2025-10-05 18:49:00 +0530

    init commit

--- src/main/java/org/aop/aopconcepts/crosscuttingconcerns/LoggingAspect.java
+++ src/main/java/org/aop/aopconcepts/crosscuttingconcerns/LoggingAspect.java
@@ -1,6 +1,7 @@
 package org.aop.aopconcepts.crosscuttingconcerns;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -29,5 +30,21 @@ public class LoggingAspect {
     @AfterThrowing(pointcut = "serviceMethods()", throwing = "error")
     public void logAfterThrowing(JoinPoint joinPoint,  Throwable error) {
         System.out.println("After Throwing: "+ joinPoint.getSignature().getName() + " Error: "+ error);
+    }
+
+    @Around("execution(* org.aop.aopconcepts.restcontroller.*.*(..))")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+
+        System.out.println("Before executing: " + joinPoint.getSignature().getName());
+
+        // Execute actual method
+        Object result = joinPoint.proceed();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("After executing: " + joinPoint.getSignature().getName() +
+                " | Execution time: " + (endTime - startTime) + " ms");
+
+        return result;
     }
 }
